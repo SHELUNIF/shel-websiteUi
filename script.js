231eb302726c88eb355367b7b3900a9d5fe105ae
SHELUNIF <shel.un.ind@gmail.com>
2024-06-08 11:24:07 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Aquí puedes agregar JavaScript adicional si lo necesitas 
+Dependencies lock file is not found in /home/runner/work/shel-websiteUi/shel-websiteUi. Supported file patterns: package-lock.json,npm-shrinkwrap.json,yarn.lock// Aquí puedes agregar JavaScript adicional si lo necesitas 
 // Aquí puedes agregar JavaScript adicional si lo necesitas
 
 document.getElementById('contact-form').addEventListener('submit', function(event) {
