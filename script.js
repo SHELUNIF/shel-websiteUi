6d29110c69e7611a846743c2fcdcb93e349a0ad6
SHELUNIF <shel.un.ind@gmail.com>
2024-06-08 09:23:14 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Aquí puedes agregar JavaScript adicional si lo necesitas 
+// Aquí puedes agregar JavaScript adicional si lo necesitas
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulario enviado. Nos pondremos en contacto contigo pronto.');
+});
